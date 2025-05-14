6151bfae3d0ad81d873862f7c2ba2bb49dd4c46b
pavel-iakush <lovenpeace.anim@gmail.com>
2025-05-14 18:12:21 +0300

    yohoho
    enum dropdown is working

--- Assets/Scripts/LevelBehaviour/SpawnPoint.cs
+++ Assets/Scripts/LevelBehaviour/SpawnPoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
@@ -7,20 +8,70 @@ public class SpawnPoint : MonoBehaviour
     [SerializeField] private EnemyIdleStates _enemyIdleState;
     [SerializeField] private EnemyAgroStates _enemyAgroStates;
 
-    [SerializeField] private Player _player;
+    private Player _player;
     [SerializeField] private Enemy _enemyPrefab;
+
+    private Enemy _currentEnemy;
 
     private void Awake()
     {
-        Enemy enemy = Instantiate(_enemyPrefab, gameObject.transform);
+        _player = FindObjectOfType<Player>();
+        _currentEnemy = Instantiate(_enemyPrefab, gameObject.transform);
+
+        int enemyBeh = (int)_enemyAgroStates;
+
+        SpawnWith((EnemyAgroStates)enemyBeh);
+
+        /*if (_enemyAgroStates.Equals(0))
+        {
+            RunAway runAway = new RunAway(_currentEnemy, _player);
+            _currentEnemy.Initialize(runAway);
+        }
+
+        if (_enemyAgroStates.Equals(1))
+        {
+            ChasePlayer chasePlayer = new ChasePlayer(_currentEnemy, _player);
+            _currentEnemy.Initialize(chasePlayer);
+        }
+
+        if (_enemyAgroStates.Equals(2))
+        {
+            ScareAndDie scareAndDie = new ScareAndDie(_currentEnemy, _player);
+            _currentEnemy.Initialize(scareAndDie);
+        }*/
 
         /*ChasePlayer chasePlayer = new ChasePlayer(enemy, _player);
-        enemy.Initialize(chasePlayer);*/
+        enemy.Initialize(chasePlayer);
 
-        /*RunAway runAway = new RunAway(enemy, _player);
-        enemy.Initialize(runAway);*/
+        RunAway runAway = new RunAway(enemy, _player);
+        enemy.Initialize(runAway);
 
         ScareAndDie scareAndDie = new ScareAndDie(enemy, _player);
-        enemy.Initialize(scareAndDie);
+        enemy.Initialize(scareAndDie);*/
+    }
+
+    private void SpawnWith(EnemyAgroStates enemyAgroState)
+    {
+        switch (enemyAgroState)
+        {
+            case EnemyAgroStates.RunAway:
+                RunAway runAway = new RunAway(_currentEnemy, _player);
+                _currentEnemy.Initialize(runAway);
+                break;
+
+            case EnemyAgroStates.ChasePlayer:
+                ChasePlayer chasePlayer = new ChasePlayer(_currentEnemy, _player);
+                _currentEnemy.Initialize(chasePlayer);
+                break;
+
+            case EnemyAgroStates.ScareAndDie:
+                ScareAndDie scareAndDie = new ScareAndDie(_currentEnemy, _player);
+                _currentEnemy.Initialize(scareAndDie);
+                break;
+
+            default:
+                Debug.Log("Spawn type is not supported");
+                break;
+        }
     }
 }
